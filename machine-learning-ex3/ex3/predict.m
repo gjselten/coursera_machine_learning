8dc07472819adfdc856e31800b87762318db2faf
gjselten <geert-jan@cg.nl>
2019-06-09 11:36:18 +0200

    ex3 - predict (neural network prediction)

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,12 +21,32 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% X contains 5000 x 400, which are 5000 training examples with 400 features each (the pixels)
+% Theta1: the weights to go from the first layer (input layer) to second layer (hidden layer). 25x401
+% Theta2: the weights to go from first layer to third layer (output layer) 10x26
+% p contains 5000 x 1. The expected outcome is index that corresponds to a digit class.
+%
+% The second layer has 25 units
+% The third layer has 10 units, corresponding to the 10 digit classes.
 
 
+% go from input layer to hidden layer
+Xtemp = [ones(m, 1) X];           % add ones to X
+layer_1_z = Xtemp * Theta1';      % calculate Z for hidden layer
+X2 = sigmoid(layer_1_z);          % calculate H for hidden layer. Called it 'X2', as it serves as input for the next layer.
 
+% go from hidden layer to output layer
+m2 = size(X2, 1);                    % get size of X2
+Xtemp2 = [ones(m2, 1) X2];           % add ones to X2
+layer_2_z = Xtemp2 * Theta2';        % calculate Z for hidden layer
+h = sigmoid(layer_2_z);              % calculate H for output layer
 
-
-
+% h contains a 5000 x 10 matrix. every row contains 10 elements. Every element
+% is the probably that the input is this output (eg first element = 1, etc.)
+% the highest number is the best match and therefore our prediction.
+[max_value, index_max_value] = max(h(1,:));
+[max_value, index_max_value] = max(h');
+p = index_max_value;
 
 
 % =========================================================================
